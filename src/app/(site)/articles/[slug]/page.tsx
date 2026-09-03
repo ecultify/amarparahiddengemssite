693c872088aa0c0ff42ebde755c4051f80dde2e8
abhinv98 <98abrai@gmail.com>
2026-09-03 12:42:17 +0530

    Stand the article sprout over the O of YOUR
    It was touching the cap line but reading as detached; it now rests
    into the letter's top and sits in front of the headline.

--- src/app/(site)/articles/[slug]/page.tsx
+++ src/app/(site)/articles/[slug]/page.tsx
@@ -65,7 +65,7 @@ export default async function ArticlePage({ params }: { params: Promise<{ slug: 
         />
         <Asset
           src={IMG.blogSprout}
-          className="pointer-events-none absolute top-[-2px] left-[calc(50%+104px)] h-[58px] w-[33px] lg:top-[43px] lg:left-[calc(50%-65px)] lg:h-[80px] lg:w-[49px] object-contain"
+          className="pointer-events-none absolute top-[-2px] left-[calc(50%+104px)] h-[58px] w-[33px] lg:top-[55px] lg:left-[calc(50%-65px)] lg:z-40 lg:h-[80px] lg:w-[49px] object-contain"
         />
         <Asset
           src={IMG.blogDhol}
